2fef2c597809d7fe7938e9b67bc66d49c3c7199a
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2022-11-05 18:10:49 +0700

    first project

---
+++ KasirApp/kasir-app/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import { Hasil, ListCategories, NavbarComponent } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponent />
+      <div className="mt-3">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong>Daftar Produk</strong>
+              </h4>
+              <hr />
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+}
+
+export default App;
