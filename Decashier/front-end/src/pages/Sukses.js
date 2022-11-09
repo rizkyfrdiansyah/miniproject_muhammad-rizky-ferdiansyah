404966846f80d702a6a97d83ad45bc8849621ea8
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2022-11-09 11:30:24 +0700

    update image

--- Decashier/front-end/src/pages/Sukses.js
+++ Decashier/front-end/src/pages/Sukses.js
@@ -25,7 +25,7 @@ export default class Sukses extends Component {
   render() {
     return (
       <div className="mt-4 text-center">
-        <Image src="assets/images/sukses.png" width="500" />
+        <Image src="assets/images/success.png" width="500" />
         <h2>Sukses Pesan</h2>
         <p>Terimakasih Sudah Memesan!</p>
         <Button variant="primary" as={Link} to="/">
