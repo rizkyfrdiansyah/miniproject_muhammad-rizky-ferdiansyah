2b7be82a27c97fb2699fdfd5cdec4abf875df9a2
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2022-11-20 00:24:21 +0700

    update

--- Decashier/front-end/src/pages/About.js
+++ Decashier/front-end/src/pages/About.js
@@ -1,16 +1,35 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 const About = () => {
   return (
     <div>
       <Container>
         <br />
-        <h1>About</h1>
+        <h1>About Us</h1>
         <Row>
           <Col>
-            <p>
+            <p className="about">
               Decashier adalah sebuah website yang menjalankan bisnis sebuah makanan dari berbagai jenis makanan yang ada di Indonesia. Website ini dibuat untuk membantu mempermudah customer memesan makanan melalui website ataupun mobile.
             </p>
+            <br />
+            <br />
+            <p>
+              <strong>Our Team Members</strong>
+            </p>
+
+            <Row xs={1} md={2} className="g-4">
+              {Array.from({ length: 4 }).map((_, idx) => (
+                <Col>
+                  <Card className="cashier-card">
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
           </Col>
         </Row>
       </Container>
